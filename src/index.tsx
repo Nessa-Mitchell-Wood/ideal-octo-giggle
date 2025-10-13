a03e90e46363e564cb942a055fbea1cb04dfb23f
Nessa Mitchell-Wood <empty.flute.shakuhachi@gmail>
2025-10-13 15:51:16 +1100

    added keys to env

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+
+const { DISCORD_KEY } = process.env;
 
 const app = new Hono();
 
 app.use(logger());
 
 app.get("/", (c) => c.text("Hello from nmw.art!"));
+
+app.get("/.well-known/discord", (c) => c.text(DISCORD_KEY as string));
 
 const server = serve(
   { fetch: app.fetch, port: 3000 },
